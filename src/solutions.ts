7e09208e4624582fbc419492bbe55369c4fc5c58
aklimaakhter <aklima00191@gmail.com>
2026-05-07 12:01:13 +0600

    Problem-4

--- src/solutions.ts
+++ src/solutions.ts
@@ -1,10 +1,10 @@
 //  Problem-1
 
-const filterEvenNumbers = (numbersArray: number[]):number[] => {
+const filterEvenNumbers = (numbersArray: number[]): number[] => {
     return numbersArray.filter(element => element % 2 === 0);
 }
 
-const output=(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
+const output = (filterEvenNumbers([1, 2, 3, 4, 5, 6]));
 // console.log(output)
 
 // Problem-2
@@ -12,33 +12,38 @@ const reverseString = (text: string): string => {
     return text.split("").reverse().join("");
 }
 
-const reverse=(reverseString("typescript"));
+const reverse = (reverseString("typescript"));
 // console.log(reverse)
 
 
 
 // Problem-3
-// Define a union type StringOrNumber and create a function checkType that uses type guards to return "String" if the input is a string or "Number" if the input is a number.
+type StringNumber = string | number;
 
-// Sample Input 1:
-// checkType("Hello");
-
-// Sample Output 1:
-// "String";
-
-// Sample Input 2:
-// checkType(42);
-
-// Sample Output 2:
-// "Number";
-
-type StringNumber=string|number;
-
-const checkType=(stringNumber:StringNumber)=>{
-    if(typeof stringNumber==="number"){
+const checkType = (stringNumber: StringNumber) => {
+    if (typeof stringNumber === "number") {
         return "Number"
-    }else if(typeof stringNumber==="string"){
+    } else if (typeof stringNumber === "string") {
         return "String"
     }
 }
-console.log(checkType('jg'))
+const result1 = checkType(22)
+// console.log(result1)
+const result2 = checkType("Hello")
+// console.log(result2)
+
+
+// Problem-4
+const getProperty = <X>(obj: X, key: keyof X) => {
+    return obj[key];
+}
+
+const user = {
+    id: 1,
+    name: "John Doe",
+    age: 21
+};
+const result = getProperty(user, 'name')
+console.log(result)
+
+
