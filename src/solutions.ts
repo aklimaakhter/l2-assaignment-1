537e645115b75147f30b70e262c9f0d300566fa7
aklimaakhter <aklima00191@gmail.com>
2026-05-07 11:07:56 +0600

    Problem-3

--- src/solutions.ts
+++ src/solutions.ts
@@ -13,4 +13,32 @@ const reverseString = (text: string): string => {
 }
 
 const reverse=(reverseString("typescript"));
-console.log(reverse)
+// console.log(reverse)
+
+
+
+// Problem-3
+// Define a union type StringOrNumber and create a function checkType that uses type guards to return "String" if the input is a string or "Number" if the input is a number.
+
+// Sample Input 1:
+// checkType("Hello");
+
+// Sample Output 1:
+// "String";
+
+// Sample Input 2:
+// checkType(42);
+
+// Sample Output 2:
+// "Number";
+
+type StringNumber=string|number;
+
+const checkType=(stringNumber:StringNumber)=>{
+    if(typeof stringNumber==="number"){
+        return "Number"
+    }else if(typeof stringNumber==="string"){
+        return "String"
+    }
+}
+console.log(checkType('jg'))
